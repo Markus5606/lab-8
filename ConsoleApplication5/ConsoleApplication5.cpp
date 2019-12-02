061612531272e6800cb84fb2ac762d4f75a45a05
Markus5606 <romka5606@gmail.com>
2019-12-02 13:03:57 +0200

    initial commit

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,24 @@
+﻿#include<TCHAR.H>
+#include"iostream"
+#include"windows.h"
+#include"math.h"
+
+using namespace std;
+
+int main()
+{
+	double A, x, z, y;
+
+	printf("input X: ");
+	scanf_s("%lf", &x);
+	printf("input Y: ");
+	scanf_s("%lf", &y);
+	printf("input Z: ");
+	scanf_s("%lf", &z);
+
+	A = x + y * z;
+	printf("%lf\n", A);
+		system("pause");
+
+	return 0;
+}
